659d4c64c9075c7b533a2229ed245e13681fc564
John <john.yeung@edgernd.com>
2018-05-02 19:04:37 -0700

    Add extension class and move validation to model

---
+++ John.Yeung.WsecuApplication/Models/UserExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace John.Yeung.WsecuApplication.Models
+{
+    public partial class User : IValidatableObject
+    {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var errors = new List<ValidationResult>();
+
+            if (string.IsNullOrWhiteSpace(Name))
+                errors.Add(new ValidationResult("Name is required."));
+            if (string.IsNullOrWhiteSpace(Email))
+                errors.Add(new ValidationResult("Email is required."));
+            if (string.IsNullOrWhiteSpace(UserName))
+                errors.Add(new ValidationResult("Username is required."));
+
+            return errors;
+        }
+    }
+}
